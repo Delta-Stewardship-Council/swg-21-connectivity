38d610562ff2537dd50e9d62f368ad7668335d3a
Yamane <lauren_yamane@fws.gov>
2021-10-28 16:41:44 -0700

    Added code for ggplot of 4 cyprinid species 1998 to present

--- scripts/get_djfmp_fish.R
+++ scripts/get_djfmp_fish.R
@@ -23,6 +23,35 @@ DJFMP_all <- LTMRdata::DJFMP
 # download the stations locations (if a separate file is desired)
 djfmp_stations_76_20 <- download.file("https://portal.edirepository.org/nis/dataviewer?packageid=edi.244.7&entityid=99a038d691f27cd306ff93fdcbc03b77", destfile = here("data/DJFMP_Fish_stations.csv"))
 
+# Explore beach seine data from 1998 to present
+DJFMP_seine_98_now <- DJFMP_all %>% filter(Method == "Beach seine") %>% filter(Datetime > "1997-12-31")
 
+# Look specifically at cyprinids: Sac blackfish (Orthodon microlepidotus), Sac pikeminnow (Ptychocheilus grandis), splittail (Pogonichthys macrolepidotus), and Sac sucker (Catostomus occidentalis), within latitudes 38.156314 to 39
 
+DJFMP_seine_98_now_4cyp <- DJFMP_seine_98_now %>%
+  filter(Taxa %in% c("Orthodon microlepidotus","Ptychocheilus grandis", "Pogonichthys macrolepidotus", "Catostomus occidentalis")) %>%
+  filter(Latitude > 38.156 & Latitude < 39) %>%
+  filter(Station != "SF014E")
 
+# Liberty Island
+LI_stations <- unique(DJFMP_seine_98_now_4cyp$Station[grep("LI", DJFMP_seine_98_now_4cyp$Station)])
+
+# American River
+AM_stations <- unique(DJFMP_seine_98_now_4cyp$Station[grep("AM", DJFMP_seine_98_now_4cyp$Station)])
+
+# Georgiana Slough
+GS_stations <- unique(DJFMP_seine_98_now_4cyp$Station[grep("GS", DJFMP_seine_98_now_4cyp$Station)])
+
+# Sac River
+SR_stations <- unique(DJFMP_seine_98_now_4cyp$Station[grep("SR", DJFMP_seine_98_now_4cyp$Station)])
+
+# Steamboat Slough
+SS_stations <- unique(DJFMP_seine_98_now_4cyp$Station[grep("SS", DJFMP_seine_98_now_4cyp$Station)])
+
+# Delta Cross Channel
+XC_stations <- unique(DJFMP_seine_98_now_4cyp$Station[grep("XC", DJFMP_seine_98_now_4cyp$Station)])
+
+DJFMP_seine_98_now_4cyp <- DJFMP_seine_98_now_4cyp %>%
+  dplyr::mutate(Region = dplyr::case_when(Station %in% c(LI_stations) ~ "Liberty_Island", Station %in% c(GS_stations) ~ "Georgiana_Slough", Station %in% c(AM_stations) ~ "American_River", Station %in% c(SR_stations) ~ "Sac_River", Station %in% c(SS_stations) ~ "Steamboat_Slough", Station %in% c(XC_stations) ~ "Delta_XC"))
+# plot the 4 cyprinid species over time period of interest
+cyp_plot <- ggplot(DJFMP_seine_98_now_4cyp, aes(x = Date, y = Count, group = Taxa, color = Taxa)) + geom_point() + theme_bw() + facet_wrap(vars(Region))
