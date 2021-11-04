47a753241ddc9aea256580574e506d6f87694a91
Ryan Peek <rapeek@ucdavis.edu>
2021-11-04 10:19:29 -0700

    add functions for workflows

---
+++ scripts/functions/f_get_daymet.R
@@ -0,0 +1,32 @@
+# get dayMet for a location
+
+download_daymet_data <- function(lat, lon){
+
+# Yolo
+#lats <- 38.307857513
+#lons <- -121.692428589
+
+library(daymetr)
+yolo_daymet <- download_daymet(site = "Yolo",
+                               lat = lats,
+                               lon =  lons,
+                               start = 1994,
+                               end = 2020, internal = TRUE)
+
+
+# rename variables, create a date column
+yolo_daymet_df <- yolo_daymet$data %>%
+  transmute(date = ymd(paste0(year, '01-01'))+ days(yday) -1,
+            daymet_precip_mm = prcp..mm.day., # mm ppt / day
+            daymet_tmax = tmax..deg.c., #max temp
+            daymet_tmin = tmin..deg.c., # min temp
+            daymet_tmean = (daymet_tmax + daymet_tmin) / 2, # mean temp
+            daymet_trange = daymet_tmax - daymet_tmin, # temp range
+            daymet_srad = srad..W.m.2., # soloar radiation
+            daymet_vpd = vp..Pa.)
+
+gg_miss_var(yolo_daymet_df)
+
+# save out:
+write_csv(yolo_daymet_df, "data/cimis_yolo_1994-2020.csv")
+}
---
+++ scripts/functions/f_get_verona_flow.R
@@ -0,0 +1,47 @@
+# usgs flow stations
+
+# pull data
+download_daily_flow_data <- function(gageID="11425500", param="00060") {
+
+  # param codes: discharge="00060", stage="00065" , temp="00010"
+  library(dataRetrieval)
+  library(glue)
+
+  # check info: gageID
+  # sta_info <- dataRetrieval::whatNWISsites(sites=gageID)
+
+  # check what data is available for a site:
+  # whatNWISdata(sites=gageID, service="dv") # daily
+
+  ## preview map
+  # library(sf)
+  # verona_sta <- verona_sta %>%
+  #   st_as_sf(coords=c("dec_long_va", "dec_lat_va"),
+  #            crs=4326, remove=FALSE)
+  # mapview::mapview(verona_sta)
+
+  # get data:
+  print("Downloading data...")
+  flowdat <- dataRetrieval::readNWISdv(siteNumbers = c(gageID), parameterCd = c(param))
+
+  # fix names
+  flowdat <- addWaterYear(flowdat)
+  flowdat <- dataRetrieval::renameNWISColumns(flowdat)
+
+  print("Data downloaded!")
+
+  # write out
+  write_csv(flowdat, glue("data/usgs_{gageID}_flow_{min(flowdat$waterYear)}-{max(flowdat$waterYear)-1}.csv"))
+
+  # print message!
+  print(glue("Data saved here: 'data/usgs_{gageID}_flow_{min(flowdat$waterYear)}-{max(flowdat$waterYear)-1}.csv'"))
+
+  # quick plot
+  p1 <- ggplot(flowdat) +
+    geom_line(aes(x=Date, y=Flow)) +
+    labs(title=glue("USGS Daily flow for {gageID}: {min(flowdat$waterYear)}-{max(flowdat$waterYear)}"))
+
+
+  return(p1)
+
+}
