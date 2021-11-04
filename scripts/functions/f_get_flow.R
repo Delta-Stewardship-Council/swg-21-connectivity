181bba87efe2de82963b2a16cf34a1a149d49224
Ryan Peek <rapeek@ucdavis.edu>
2021-11-04 12:28:19 -0700

    update

--- scripts/functions/f_get_flow.R
+++ scripts/functions/f_get_flow.R
@@ -1,24 +1,26 @@
-# usgs flow stations
+# Download data from USGS stations
 
-# pull data
+# param codes: discharge="00060", stage="00065" , temp="00010"
+# to check info for a given gageID:
+## sta_info <- dataRetrieval::whatNWISsites(sites=gageID)
+
+# check what data is available for a site:
+## dataRetrieval::whatNWISdata(sites=gageID, service="dv") # daily
+
+# preview map
+## library(sf)
+## verona_sta <- verona_sta %>%
+##   st_as_sf(coords=c("dec_long_va", "dec_lat_va"),
+##            crs=4326, remove=FALSE)
+## mapview::mapview(verona_sta)
+
+library(glue)
+library(dataRetrieval)
+library(dplyr)
+library(ggplot2)
+
+# pull data: defaults to Verona daily discharge
 download_daily_flow_data <- function(gageID="11425500", param="00060") {
-
-  # param codes: discharge="00060", stage="00065" , temp="00010"
-  library(dataRetrieval)
-  library(glue)
-
-  # check info: gageID
-  # sta_info <- dataRetrieval::whatNWISsites(sites=gageID)
-
-  # check what data is available for a site:
-  # whatNWISdata(sites=gageID, service="dv") # daily
-
-  ## preview map
-  # library(sf)
-  # verona_sta <- verona_sta %>%
-  #   st_as_sf(coords=c("dec_long_va", "dec_lat_va"),
-  #            crs=4326, remove=FALSE)
-  # mapview::mapview(verona_sta)
 
   # get data:
   print("Downloading data...")
@@ -37,11 +39,9 @@ download_daily_flow_data <- function(gageID="11425500", param="00060") {
   print(glue("Data saved here: 'data/usgs_{gageID}_flow_{min(flowdat$waterYear)}-{max(flowdat$waterYear)-1}.csv'"))
 
   # quick plot
-  library(ggplot2)
   p1 <- ggplot(flowdat) +
     geom_line(aes(x=Date, y=Flow)) +
     labs(title=glue("USGS Daily flow for {gageID}: {min(flowdat$waterYear)}-{max(flowdat$waterYear)}"))
-
 
   return(p1)
 
