67a140d478042543fbb65bc112389f5b7ccff394
Shruti Khanna <shrkhanna@ucdavis.edu>
2021-11-04 15:15:00 -0700

    get function for cimis-bryte

---
+++ scripts/functions/f_get_temp_srad_cimis.R
@@ -0,0 +1,34 @@
+# Load CIMIS data that has been downloaded locally
+
+## Meanings of quality flags
+# R far out than historical max
+# M missing
+# Y out of historical range
+# P
+# H any solar radiation flagged M/S/Q
+# S extreme solar radiation
+
+library(glue)
+library(dplyr)
+library(ggplot2)
+library(janitor)
+
+# pull data: defaults to Verona daily discharge
+f_get_temp_srad_cimis <- function(stationID="Bryte") {
+
+  # get data:
+  cimis <- read.csv("data/Bryte_CIMIS_daily.csv")
+
+  print("Data downloaded!")
+
+  # clean column names
+  cimis <- clean_names(cimis)
+
+  # write out
+  readr::write_csv(cimis, glue("data_raw/raw_temp_srad_cimis_{stationID}.csv"))
+
+  # print message!
+  print(glue("Data saved here: 'data_raw/raw_temp_srad_cimis_{stationID}.csv'"))
+
+  return(cimis)
+}
