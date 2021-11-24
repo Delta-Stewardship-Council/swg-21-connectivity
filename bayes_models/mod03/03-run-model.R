289a489645f133a8a6f4c8f361d81a8e2a280205
Lauren Yamane <yamane.lauren@gmail.com>
2021-11-24 11:06:46 -0800

    mod03 with inund_days, qant, Rad

---
+++ bayes_models/mod03/03-run-model.R
@@ -0,0 +1,166 @@
+# Create input for SAM model
+# Replace past_topped with inund_days, subtract Rant and Tant, use Rad
+# Accounts for random effect of site (intercept-only, due to low sample size)
+
+
+# Libraries ---------------------------------------------------------------
+
+library(readr)
+library(dplyr)
+library(rjags)
+library(mcmcplots)
+library(broom.mixed)
+library(ggplot2)
+library(naniar)
+library(lubridate)
+#devtools::install_github("fellmk/PostJAGS/postjags")
+library(postjags)
+
+
+
+# Bring in Data For Model -------------------------------------------------
+
+source("scripts/functions/f_make_bayes_mod03_dataset.R")
+mod_df <- f_make_bayes_mod03_dataset()
+
+# pull out datasets
+chla_all <- mod_df$chla_all
+covars <- mod_df$covars
+
+# Visualize Data ----------------------------------------------------------
+
+# check histogram of logged chlorophyll
+hist(log(chla_all$chl))
+
+# check sd among site mean chlorophyll, set as parameter for folded-t prior
+sd(tapply(chla_all$chl, chla_all$station_id, mean))
+
+
+# Create Model Datalist ---------------------------------------------------
+
+datlist <- list(chl = log(chla_all$chl),
+                inund_days = c(covars$inund_days),
+                Q = c(covars$Q),
+                Rad = c(covars$Rad),
+                station_id = chla_all$station_id,
+                Nstation = length(unique(chla_all$station_id)),
+                doy1998 = chla_all$doy1998,
+                N = nrow(chla_all),
+                pA = c(1, 3, 3, 7, 7),
+                # mean of 1 day, mean of 3 days before that, etc
+                nlagA = 5, # index for for loop
+                alphaA = rep(1, 5)) # for prior
+
+
+# Set up Initial Model Starts ---------------------------------------------
+
+# Initials functions for root node parameters
+inits <- function(){
+  list(sig.eps = runif(1, 0, 15),
+       tau = runif(1, 0, 1),
+       B = rnorm(5, 0, 1000)) # for 5 B parameters, adjust as needed
+}
+initslist <- list(inits(), inits(), inits())
+
+# run this if model has been successfully run already:
+
+# Or load saved.state
+#load("bayes_models/mod01/inits/sstate.Rda")
+#initslist <- saved.state[[2]]
+
+
+# Run Model ---------------------------------------------------------------
+
+# Run model
+jm <- jags.model("bayes_models/mod03/sam_model.JAGS",
+                 data = datlist,
+                 inits = initslist,
+                 n.chains = 3)
+
+update(jm, n.iter = 3000) #1000 with no thinning looked bad
+
+# Sample Posterior
+jm_coda <- coda.samples(jm,
+                        variable.names = c("Bstar", "wA",
+                                           "deltaA",
+                                           "sig", "tau", "sig.eps", "tau.eps",
+                                           "Estar"),
+                        n.iter = 3000*15, #*15
+                        thin = 15)
+
+
+
+# Load Saved Model --------------------------------------------------------
+
+#load("bayes_models/mod01/run_20211124.rda")
+
+# Visualize
+mcmcplot(jm_coda)
+
+# Save state for rerunning
+newinits <- initfind(coda = jm_coda)
+newinits[[1]]
+saved.state <- removevars(initsin = newinits, variables=c(1:5, 7, 9:11))
+save(saved.state, file = "bayes_models/mod03/inits/sstate.Rda")
+
+
+# Look at Outputs ---------------------------------------------------------
+
+# betas
+caterplot(jm_coda,
+          regex  = "Bstar",
+          reorder = FALSE)
+
+# stations
+caterplot(jm_coda,
+          parms = "Estar",
+          reorder = FALSE)
+
+# time lags
+caterplot(jm_coda,
+          parms = "wA",
+          reorder = FALSE)
+
+# summarize and plot
+coda_sum <- tidyMCMC(jm_coda,
+                     conf.int = TRUE,
+                     conf.level = 0.95,
+                     conf.method = "HPDinterval")
+
+# intercept (interpreted as log(chlA) at average conditions, since covariates are standardized)
+coda_sum %>%
+  filter(grepl("Bstar\\[1", term)) %>%
+  ggplot(aes(x = term, y = estimate)) +
+  geom_hline(yintercept = 0, color = "red") +
+  geom_pointrange(aes(ymin = conf.low, ymax = conf.high)) +
+  scale_y_continuous("Intercept") +
+  theme_bw()
+
+
+# slope of inund_days, Q, Qant, Rad
+# can interpret relative influence of each covariate, since covariates are standardized
+coda_sum %>%
+  filter(grepl("Bstar\\[[2-5]{1}", term)) %>%
+  ggplot(aes(x = term, y = estimate)) +
+  geom_hline(yintercept = 0, color = "red") +
+  geom_pointrange(aes(ymin = conf.low, ymax = conf.high)) +
+  scale_x_discrete(labels = c("inund_days", "Q", "Qant", "Rad")) +
+  scale_y_continuous("Slope of covariates") +
+  theme_bw()
+ggsave(filename = "bayes_models/mod03/fig_out/slope_of_betas_inund_qant_sol_temp.png",
+       dpi=300, width = 10, height = 8)
+
+# weights of Qant
+coda_sum %>%
+  filter(grepl("wA", term)) %>%
+  ggplot(aes(x = term, y = estimate)) +
+  geom_hline(yintercept = 1/5, color = "red") +
+  geom_pointrange(aes(ymin = conf.low, ymax = conf.high)) +
+  scale_y_continuous("Weights of past Q") +
+  theme_bw()
+ggsave(filename = "bayes_models/mod03/fig_out/weights_of_qant.png",
+       dpi=300, width = 10, height = 8)
+
+
+# save model
+save(jm_coda, coda_sum, file = "bayes_models/mod03/run_20211124.rda")
