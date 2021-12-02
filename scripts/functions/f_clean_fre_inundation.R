e4eba37d9ad82d0dc625e33177defeacc533a283
Ryan Peek <rapeek@ucdavis.edu>
2021-12-02 09:10:46 -0800

    update and clean

--- scripts/functions/f_clean_fre_inundation.R
+++ scripts/functions/f_clean_fre_inundation.R
@@ -3,14 +3,15 @@ library(dplyr)
 library(imputeTS)
 library(here)
 
-# source("scripts/functions/f_get_fre.R")
-# fre <- f_get_fre() # takes a minute to run
-
-# convert below to a function:
+source(here("scripts/functions/f_get_dayflow.R"))
 
 f_clean_fre_inundation <- function(start='1995-02-23', end='2021-01-01'){
 
-  # load fremont wier stage data
+  # load fremont wier stage data directly from csv.gz
+  # assumes this has been run already:
+  # source("scripts/functions/f_get_fre.R")
+  # f_get_fre() # takes a few minutes to run
+
   fre <- read.csv(here("data_raw/raw_stage_fre.csv.gz"))
   fre$date <- as.Date(fre$datetime)
 
@@ -39,51 +40,46 @@ f_clean_fre_inundation <- function(start='1995-02-23', end='2021-01-01'){
                                 date = seq(as.Date(start),as.Date(end),by='day'))
 
   print("Filling missing dates with 7 day moving average...")
-  # add to dataset
+  ## Merge datasets --------------
   discharge_sac_na <- merge(discharge_sac, continous.dates, by = "date", all = TRUE)
-  # use library(imputeTS)
+
   # now impute missing data based on 7 day weighted moving average
   discharge_sac_na$height_sac_na <- na_ma(discharge_sac_na$height_sac, k = 7, weighting = "exponential", maxgap = Inf)
 
-  ### clean dayflow
-  # load dayflow
-  source(here("scripts/functions/f_load_dayflow.R"))
-  dayflow <- f_load_dayflow()
+  ## Get dayflow ----------------
+  dayflow <- f_get_dayflow()
   dayflow$date <- as.Date(dayflow$Date, "%m/%d/%Y")
 
   # merge two water datasets
-  All_flows <- merge(dayflow[,c(5,30)], discharge_sac_na[,c(1,4)], by = "date")
+  all_flows <- merge(dayflow[,c(5,30)], discharge_sac_na[,c(1,4)], by = "date")
   check_again <- seq(as.Date('1996-10-01'), as.Date('2020-09-30'), by = 'day')
 
-  # nrow(All_flows)==length(check_again) # no missing dates!!!
+  ## calculate inundation days ---------------------
 
-  ### calculate inundation days
-  # definition for inundation days
   print("Now looping through and calculating inundation based on height (33.5 ft)...")
-  for(i in 1:nrow(All_flows)){
+  for(i in 1:nrow(all_flows)){
     #f_clean_indundation <- function(i){
-    if(All_flows[i,"height_sac_na"] < 33.5){
-      All_flows[i,"inund_days"] <- 0}
-    else if(All_flows[i, "height_sac_na"] >= 33.5){
-      All_flows[i, "inund_days"] <- All_flows[i-1, "inund_days"]+1}
+    if(all_flows[i,"height_sac_na"] < 33.5){
+      all_flows[i,"inund_days"] <- 0}
+    else if(all_flows[i, "height_sac_na"] >= 33.5){
+      all_flows[i, "inund_days"] <- all_flows[i-1, "inund_days"]+1}
     else {
-      All_flows[i, "inund_days"] <- 0 }
+      all_flows[i, "inund_days"] <- 0 }
+    # jessica's addition to fix the tails
     if(i > 1) {
-      All_flows[i, "yolo"] >= 4000 & All_flows[i-1, "inund_days"] > 0
-      All_flows[i, "inund_days"] <- All_flows[i-1, "inund_days"]+1
+      all_flows[i, "yolo"] >= 4000 & all_flows[i-1, "inund_days"] > 0
+      all_flows[i, "inund_days"] <- all_flows[i-1, "inund_days"]+1
     }
   }
 
-  # this is returning an error when run as a function (see line 45), just keep as loop
-  # lapply(seq(1, nrow(All_flows)), f_clean_indundation)
-  # Error in x[[jj]][iseq] <- vjj : replacement has length zero
-
   ### add column for inundation: Flooding = yes (1), or no (0)
-  All_flows$inundation <- ifelse(All_flows$inund_days > 0, 1, 0)
+  all_flows$inundation <- ifelse(all_flows$inund_days > 0, 1, 0)
+
+  ## Save out --------------
 
   print("Data cleaned and run...now saving!")
-  write.csv(All_flows, "data_clean/clean_fre_inundation_days.csv", row.names = FALSE)
+  write.csv(all_flows, "data_clean/clean_fre_inundation_days.csv", row.names = FALSE)
 
   print("Done!")
-  return(All_flows)
+  return(all_flows)
 }
