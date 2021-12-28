8c29a8bc7f693660b8bb9089ba925188ee3cb206
Lauren Yamane <yamane.lauren@gmail.com>
2021-12-27 16:28:29 -0800

    Plots of cross correlated posteriors

--- bayes_models/mod06/06-run-model.R
+++ bayes_models/mod06/06-run-model.R
@@ -90,13 +90,13 @@ initslist <- list(inits(), inits(), inits())
 # run this if model has been successfully run already:
 
 # Or load saved.state
-load("bayes_models/mod06/inits/sstate.Rda")
+load("bayes_models/mod06/inits/sstate_20211220.Rda")
 inits_2 <- function(){
   list(sig.eps = runif(1, 0, 1),
        tau = runif(1, 0, 1),
        B = rnorm(5, 0, 1)) # for 3 B parameters, adjust as needed
 }
-initslist <- list(list(sig.eps = saved.state[[2]][[2]]$sig.eps, tau = saved.state[[2]][[2]]$tau, B = inits_2()$B), list(sig.eps = saved.state[[2]][[3]]$sig.eps, tau = saved.state[[2]][[3]]$tau, B = inits_2()$B), inits_2())
+initslist <- list(list(sig.eps = saved.state[[2]][[2]]$sig.eps, tau = saved.state[[2]][[2]]$tau, B = inits_2()$B), list(sig.eps = saved.state[[2]][[1]]$sig.eps, tau = saved.state[[2]][[1]]$tau, B = inits_2()$B), inits_2())
 
 # Run Model ---------------------------------------------------------------
 
@@ -123,10 +123,41 @@ library(jagsUI)
 # Summary of marginal posterior distributions
 ( out <- jags(datlist, initslist, wanted, "bayes_models/mod06/sam_model.JAGS", DIC=FALSE,
               n.chains=3, n.adapt=100, n.iter=1000, n.burnin=0) )
+# Calculate cross correlations among variables from MCMC output
 library(coda)
-crosscorr(out$samples)
-with(out$sims.list, plot(Bstar[,3], Bstar[,4]))
-with(out$sims.list, plot(Bstar[,5], Bstar[,2]))
+crosscorr(out$samples) # we see that the posteriors of Srad_mwk and Wtemp_RIV_mwk are negatively correlated (-0.79), and
+# those for Q and inund_days are negatively correlated (-0.63)
+
+# Plot correlation in posterior between Srad_mwk and Wtemp_RIV_mwk
+png("bayes_models/mod06/fig_out/corr_posteriors_Srad_Wtemp.png",
+    height = 5, width = 6, units = "in", res = 300)
+par(cex = 1.25)
+with(out$sims.list, plot(Bstar[,3], Bstar[,4], xlab = "Srad_mwk", ylab = "Wtemp_RIV_mwk"))
+dev.off()
+
+# Plot correlation in posterior between Q and inund_days
+png("bayes_models/mod06/fig_out/corr_posteriors_Q_inund_days.png",
+    height = 5, width = 6, units = "in", res = 300)
+par(cex = 1.25)
+with(out$sims.list, plot(Bstar[,5], Bstar[,2], xlab = "Q", ylab = "inund_days"))
+dev.off()
+
+# Look at the sum of their posterior and compare to the individual posteriors
+# Srad and Wtemp
+png("bayes_models/mod06/fig_out/dens_posterior_SradWtemp.png",
+    height = 5, width = 6, units = "in", res = 300)
+par(cex = 1.25)
+sum_SradWtemp <- out$sims.list$Bstar[,3] + out$sims.list$Bstar[,4]
+dens( sum_SradWtemp , lwd=2 , xlab="sum of Srad and Wtemp" )
+dev.off()
+
+# Q and inund_days
+png("bayes_models/mod06/fig_out/dens_posterior_Qinund.png",
+    height = 5, width = 6, units = "in", res = 300)
+par(cex = 1.25)
+sum_Qinund <- out$sims.list$Bstar[,2] + out$sims.list$Bstar[,5]
+dens( sum_Qinund , lwd=2 , xlab="sum of Q and inund" )
+dev.off()
 
 mu <- with(out$sims.list, Bstar[,1] +
              Bstar[,2] * mean(out$sims.list$Bstar[,2]) +
@@ -140,7 +171,7 @@ out$sd$Bstar[4] / out$mean$Bstar[4]
 sd(mu) / mean(mu)
 # Load Saved Model --------------------------------------------------------
 
-#load("bayes_models/mod05/run_20211207.rda")
+load("bayes_models/mod06/run_20211220.rda")
 
 # Visualize
 mcmcplot(jm_coda, col = c("red", "blue", "green"))
@@ -151,7 +182,7 @@ gelman.diag(jm_coda, multivariate = FALSE)
 newinits <- initfind(coda = jm_coda)
 #saved.state <- removevars(initsin = newinits, variables=c(2:5, 8, 9))
 saved.state <- newinits
-save(saved.state, file = "bayes_models/mod05/inits/sstate_20211207.Rda")
+save(saved.state, file = "bayes_models/mod06/inits/sstate_20211220.Rda")
 
 
 # Look at Outputs ---------------------------------------------------------
@@ -193,7 +224,7 @@ coda_sum %>%
   ggplot(aes(x = term, y = estimate)) +
   geom_hline(yintercept = 0, color = "red") +
   geom_pointrange(aes(ymin = conf.low, ymax = conf.high)) +
-  scale_x_discrete(labels = c("Q", "Qant", "Srad_mwk", "Wtemp_mwk", "Inund_days")) +
+  scale_x_discrete(labels = c("Q", "Srad_mwk", "Wtemp_mwk", "Inund_days")) +
   scale_y_continuous("Slope of covariates") +
   theme_bw()
 ggsave(filename = "bayes_models/mod06/fig_out/slope_of_betas_qant_sol_wtemp_inund_20211217.png",
@@ -212,4 +243,4 @@ ggsave(filename = "bayes_models/mod05/fig_out/weights_of_qant_20211207.png",
 
 
 # save model
-save(jm_coda, coda_sum, file = "bayes_models/mod06/run_20211218.rda")
+save(jm_coda, coda_sum, file = "bayes_models/mod06/run_20211220.rda")
