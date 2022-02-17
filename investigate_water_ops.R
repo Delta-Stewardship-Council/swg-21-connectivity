7338fd4095f6ae499f802022faa582714d5001ef
goertler <pascalegoertler@Pascales-MacBook-Air.local>
2022-02-16 16:09:18 -0800

    save plots

--- investigate_water_ops.R
+++ investigate_water_ops.R
@@ -15,8 +15,13 @@ all_flows_test$pch <- ifelse(all_flows_test$cooks > cutoff, 1, 2)
 all_flows_test$test <- all_flows_test$YOLO/all_flows_test$SAC
 mean(all_flows_test$test) # 0.02765974
 
-plot(all_flows_test$SAC, all_flows_test$YOLO, xlim = c(0,77500), ylim = c(0,19083), col = all_flows_test$col, pch = all_flows_test$pch)
+tiff(filename = "figures/sac_vs_yolo.tiff", width = 10, height = 8, units = "in", res = 300)
+
+plot(all_flows_test$SAC, all_flows_test$YOLO, xlim = c(0,77500), ylim = c(0,19083), col = all_flows_test$col, pch = all_flows_test$pch, main = "Yolo flow is roughly 3% of Sac River")
 abline(a=0,b=0.03,col="green",lwd=3) #Yolo flow is roughly 3% of Sac River
+legend("topleft", c("above average", "below avrage", "Cook's outlier", "not outlier"), pch = c(0,0,1,2), col = c("red", "blue", "black", "black"))
+
+dev.off()
 
 summary(lm(all_flows_test$YOLO~all_flows_test$SAC)) # Adjusted R-squared:  0.3662
 
@@ -29,13 +34,30 @@ num <- subset(all_flows_test, outs == "TRUE") #467
 
 library(car)
 lm <- lm(all_flows_test$YOLO~all_flows_test$SAC)
+
+tiff(filename = "figures/qqplot_lm_sac_vs_yolo.tiff", width = 10, height = 8, units = "in", res = 300)
+
 qqPlot(lm)
+
+dev.off()
+
 states = row.names(all_flows_test)
 cutoff <- 4/(nrow(all_flows_test)-length(lm$coefficients)-1) #4/n-k-1
+
+tiff(filename = "figures/cooks_dist_sac_vs_yolo.tiff", width = 10, height = 8, units = "in", res = 300)
+
 plot(lm, which = 4, cook.levels = cutoff)
 abline(h = cutoff, lty = 2, col = "red")
 
+dev.off()
+
+tiff(filename = "figures/influence_sac_vs_yolo.tiff", width = 10, height = 8, units = "in", res = 300)
+
 influencePlot(lm, id.method = "identify", main = "inflence plot", sub = "circle size is proportional to Cook's distance")
+
+dev.off()
+# row 6896 is 2015-08-18
+# row 7549 is 2017-06-01
 
 # ID with Cook's distance
 all_flows_test$cooks <- cooks.distance(lm)
