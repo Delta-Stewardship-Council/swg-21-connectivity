0690aafa793560975bf4a16893aa123e7d11ccd0
Goertler <Pascale.Goertler@deltacouncil.ca.gov>
2022-05-18 15:29:42 -0700

    add total inundation days in the previous water year

--- scripts/inundation_metrics.R
+++ scripts/inundation_metrics.R
@@ -1,8 +1,8 @@
 ### this code compiles all the variations on inundation for the days that chl-a was collected
 ## 1 - inundation duration (inund_days from inundation package)
 ## 2 - inundation yes/no (inundation from inundation package)
-## 3 - days since last inundation (day count for no inundation dates and zero for inundsation dates)
-## 4 - inundation duration on an anual scale (will only vary from 1 if multiple events within one year)
+## 3 - days since last inundation (day count for no inundation dates and zero for inundation dates)
+## 4 - inundation duration on an annual scale (will only vary from 1 if multiple events within one year)
 ## 5 - total inundation in the previous water year
 
 # bring in latest inundation data
@@ -35,7 +35,27 @@ summary <- calc_summary()
 # Jeanette's water year code
 dates.posix <- as.POSIXlt(inun_metrics$date)
 offset <- ifelse(dates.posix$mon >= 10 - 1, 1, 0)
-inun_metrics$water_year <- dates.posix$year + 1900 + offset
+inun_metrics$match_water_year <- dates.posix$year + 1900 + offset
 
 # 5
-inun_metrics$total_inund_last_year
+summary$match_water_year <- summary$water_year+1
+inun_metrics <- merge(inun_metrics, summary[,c(2,8)], by = "match_water_year")
+colnames(inun_metrics)[8] <- "total_inund_last_year"
+colnames(inun_metrics)[1] <- "water_year"
+
+# 4
+inun_metrics <- merge(inun_metrics, summary[,c(1,7)], by = "water_year")
+unique(inun_metrics$number_overtopping_events)
+inun_metrics$number_overtopping_events <- ifelse(is.na(inun_metrics$number_overtopping_events), 0,
+                                                 inun_metrics$number_overtopping_events)
+
+# get dates when more than one event in a water year
+inun_metrics_with <- subset(inun_metrics, number_overtopping_events > 1)
+inun_metrics_without$days_of_inundation_until_now
+# count previous flooding event in inund_days
+
+# dates without
+inun_metrics_without <- subset(inun_metrics, number_overtopping_events < 2)
+inun_metrics_without$days_of_inundation_until_now <- inun_metrics_without$inund_days
+
+# 3
