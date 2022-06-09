e729054774752e628675b0bcb85209d6f711a6cb
catarfish <catarina.pien@gmail.com>
2022-06-08 17:45:47 -0700

    New version of data with flow and nutrients

--- scripts/functions/f_load_model_chla_covars_data.R
+++ scripts/functions/f_load_model_chla_covars_data.R
@@ -17,26 +17,49 @@ f_load_model_chla_covars_data <- function() {
   # Read data ---------------------------------------------------------
   chla_nuts_id <- contentid::store("data_model/model_chla_nuts_prioritized.csv")
   chla_nuts_file <- contentid::resolve(chla_nuts_id)
+  chla_nuts0 <- readr::read_csv(chla_nuts_file) %>%
+    rename(region = location)%>%
+    mutate(unique_id = paste0(date, "_", station_wq_chl)) %>%
+    select(-method)%>%
+    mutate(latitude = case_when(station_wq_chl == "USGS-382006121401601"~ 38.33501,
+                                station_wq_chl == "USGS-11455315"~ 38.24301,
+                                station_wq_chl == "USGS-11447650" ~ 38.45602,
+                                station_wq_chl == "USGS-11455139" ~ 38.36514,
+                               TRUE ~ latitude),
+            longitude = case_when(station_wq_chl == "USGS-382006121401601"~ -121.6711,
+                       station_wq_chl == "USGS-11455315"~ -121.6843,
+                       station_wq_chl == "USGS-11447650" ~ -121.5013,
+                         station_wq_chl == "USGS-11455139" ~ -121.6377,
+                       TRUE ~ longitude))
+  # fill in lat/lon
+
+  # nutrients
+  nuts_id <- contentid::store("data_model/nutrient_pca_results_for_gam.csv")
+  nuts_file <- contentid::resolve(nuts_id)
+  nuts <- readr::read_csv(nuts_file) %>%
+    select(din, diss_orthophos, unique_id)
+
+  # covars
   covars_id <- contentid::store("data_model/model_data_daily_covars.csv")
   covars_file <- contentid::resolve(covars_id)
-  chla_nuts0 <- readr::read_csv(chla_nuts_file) %>%
-    rename(region = location)
   covars <- readr::read_csv(covars_file)
   summary(covars)
 
   # Rearrange data for regions----------------------------------------
 
-    # remove nutrients for now
+    # remove original nutrients and replace with new
     chla_only <- chla_nuts0 %>%
       select(-c(diss_ammonia_sign:diss_silica))
 
+    chla_nuts <- left_join(chla_only,  nuts, by = "unique_id")
+
     ## * covars ----
     covars_long <- covars %>%
-    pivot_longer(cols = c(Q_above, Q_yolo, Q_below,  WTmwk_below, WTmwk_above, WTmwk_yolo, WTmwk_cache, WTrangemwk_below, WTrangemwk_above, WTrangemwk_yolo, WTrangemwk_cache, Sradmwk_above, Sradmwk_yolo, Sradmwk_below, Sradmwk_cache,
+    pivot_longer(cols = c(Q_above, Q_yolo, Q_below, Q_cache, WTmwk_below, WTmwk_above, WTmwk_yolo, WTmwk_cache, WTrangemwk_below, WTrangemwk_above, WTrangemwk_yolo, WTrangemwk_cache, Sradmwk_above, Sradmwk_yolo, Sradmwk_below, Sradmwk_cache,
                           Tmwk_above, Tmwk_yolo, Tmwk_below, Tmwk_cache, precip_yolo, precip_below, precip_above, precip_cache), names_to = c(".value","region"), names_sep =  "_") %>%
       mutate(inund_days = ifelse(region == "above", 0, inund_days),
              inundation = ifelse(region == "above", 0, inundation)) %>%
-      select(-station_flow, -agency_cd, -stage_SRV, -height_sac, -sac_dayflow, -daymet_tmax_yolo, -daymet_tmax_below, -daymet_tmax_above, -daymet_tmax_cache,
+      select(-station_flow, -agency_cd, -stage_SRV, -height_sac, -sac, -daymet_tmax_yolo, -daymet_tmax_below, -daymet_tmax_above, -daymet_tmax_cache,
              -daymet_srad_yolo, -daymet_srad_below, -daymet_srad_above, -daymet_srad_cache) %>%
       rename(Q_sday = Q,
              precip_mm = precip)
@@ -47,9 +70,9 @@ f_load_model_chla_covars_data <- function() {
 
   # join data -------------------------------------------------------
   # left_join for gam
-  chla_covars <- left_join(chla_only, covars_clean) %>%
+  chla_covars <- left_join(chla_nuts, covars_clean) %>%
     filter(!is.na(chlorophyll)) %>%
-   dplyr::filter(date>as.Date("1999-02-22") & date<as.Date("2020-01-01")) # this is when RIV starts
+   dplyr::filter(date>as.Date("1999-02-22") & water_year<2020) # this is when RIV starts
 
   # Full join for bayes
   chla_covars_fulljoin <- full_join(covars_clean, chla_only)
