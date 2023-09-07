a381aa6f7c28b8b867e18441195dce15cd3622b0
Goertler <Pascale.Goertler@water.ca.gov>
2023-09-07 14:21:58 -0700

    edits per Shruti's request for larger text in prediciton plot

--- scripts/theme_visualizations.R
+++ scripts/theme_visualizations.R
@@ -2,11 +2,11 @@ library(ggplot2)
 
 theme_vis <- theme_bw() +
   theme(strip.background = element_rect(color = "black", fill = "white"),
-        axis.text = element_text(size = 10),
-        axis.title = element_text(size = 10),
-        strip.text = element_text(size = 11),
-        legend.text = element_text(size = 10),
-        legend.title = element_text(size = 10),
+        axis.text = element_text(size = 17),
+        axis.title = element_text(size = 17),
+        strip.text = element_text(size = 18),
+        legend.text = element_text(size = 17),
+        legend.title = element_text(size = 17),
         legend.position = "top",
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank())
